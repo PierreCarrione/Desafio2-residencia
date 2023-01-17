758d8f7084f11a2067489db85f4fae149f4df83c
Pierre Carrione <50272580+PierreCarrione@users.noreply.github.com>
2023-01-17 16:14:12 -0300

    Update SiglasValidas.cs

--- ConversorMoedasAPIRest/SiglasValidas.cs
+++ ConversorMoedasAPIRest/SiglasValidas.cs
@@ -5,7 +5,7 @@ namespace ConversorMoedasAPIRest
     public class SiglasValidas//Classe que servirá para verificação das siglas permitidas.
     {
         public string Siglas { get; private set; }
-        private readonly string path = "SiglasMoedas.txt";
+        private readonly string path = "SiglasMoedas.txt";//As siglas foram retiradas desses site https://pt.iban.com/currency-codes
 
         public SiglasValidas()
         {
